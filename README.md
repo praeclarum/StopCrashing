186ab3d9451f7296f9cbca73d043a6f2f5180dfc
Frank A. Krueger <fak@praeclarum.org>
2014-11-07 08:23:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# StopCrashing.fsx
+
+This script looks for possible ways the OS can call **into** your code. These entrypoints are the potential origins for crashes because there is no useful exception handler on the stack at that time.
+
+What are these calls? Well, there are a lot of them:
+
+1. Any time you override members of UI objects
+2. Any time you export functions to the OS (as in, the responder chain)
+3. Any callbacks you register with the OS
+4. I'm sure there are more...
+
+This script uses [Mono.Cecil][MC] to scan all the byte code of your application to find these entrypoints. (Well, I've only implemented #1 and #2. #3 is a harder problem to detect.)
+
+It then detects whether you have any exception handling in that code. If you do not, it yells at you.
+
+The idea is that we don't want to bloat our software with exception handlers everywhere. This certainly makes coding more tedious but also introduces a lot of ambiguity. Instead, we still want to fail fast by leaving the majority of our code free of handlers. We will only catch exceptions where we must do so to prevent crashes.
+
+## Running
+
+Just pass it the path of your solution and it will go find the latest binaries automatically.
+
+It also returns an exit code so that it can be easily integrated into your CI server. (It is currently configured only to detect iOS and OS X calls. Android and Win RT support can be added if anyone is interested.)
+
+For example:
+
+`fsharpi --exec StopCrashing.fsx Path/To/MySolution.sln`
+
+This will find a binary for every project in `MySolution.sln` and report its bad methods.
---
+++ StopCrashing.fsx
@@ -0,0 +1,149 @@
+﻿// StopCrashing.fsx by Frank A. Krueger @praeclarum
+
+#r "System.Xml"
+#r "Mono.Cecil.dll"
+
+open System
+open System.Xml
+open System.Text.RegularExpressions
+open System.IO
+open Mono.Cecil
+open Mono.Cecil.Cil
+
+let rec diagnoseBinary (binPath : string) =
+    let res = DefaultAssemblyResolver ()
+    res.AddSearchDirectory "/Library/Frameworks/Xamarin.Mac.framework/Versions/Current/lib/mono"
+    res.AddSearchDirectory "/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/2.1"
+    let rps = ReaderParameters ()
+    rps.AssemblyResolver <- res
+    let m = ModuleDefinition.ReadModule (binPath, rps)
+    m.Types |> Seq.filter isUITypeDesc |> Seq.collect diagnoseType
+
+and diagnoseType t =
+    t.Methods |> Seq.filter isUIMethod |> Seq.filter isBadMethod
+
+and isBadMethod (m : MethodDefinition) : bool =
+    (not m.IsConstructor) && (callsOtherMethods m.Body) && (not m.Body.HasExceptionHandlers)
+
+and callsOtherMethods body =
+    body.Instructions |> Seq.exists (fun i ->
+        match i.OpCode.Code with
+        | Code.Call | Code.Calli | Code.Callvirt -> true
+        | _ -> false)
+
+and isUIMethod (m : MethodDefinition) =
+    (isExportMethod m) || (isVirtualUIMethod m)
+
+and isVirtualUIMethod (m : MethodDefinition) =
+    if not m.IsVirtual then false
+    else
+        let dt = m.DeclaringType
+        if isUIType dt then true
+        else
+            let bt = dt.BaseType.Resolve ()
+            match bt.Methods |> Seq.tryFind (fun x -> x.Name = m.Name) with
+            | Some bm -> isVirtualUIMethod bm
+            | None -> false
+
+and isExportAttr (m : CustomAttribute) =
+    m.Constructor.DeclaringType.Name = "ExportAttribute"
+
+and isExportMethod (m : MethodDefinition) =
+    match m.CustomAttributes |> Seq.tryFind isExportAttr with
+    | Some _ -> true
+    | _ -> false
+
+and isUIType (t : TypeReference) =
+    if t = null then false
+    else
+        let n = t.FullName
+        n.StartsWith ("MonoMac.AppKit.")
+        || n.StartsWith ("MonoTouch.UIKit.")
+        || n.StartsWith ("AppKit.")
+        || n.StartsWith ("UIKit.")
+
+and isUITypeDesc (t : TypeReference) =
+    if t = null then false
+    else (isUIType t) || (isUIType (t.Resolve ()).BaseType)
+
+
+//
+// Find binaries for a solution
+//
+
+let diagnoseSln slnPath =
+
+    let dump name arg = printfn "%s = %s" name (sprintf "%A" arg); arg
+
+    let projLineRe = Regex ("""Project\(.*?\)\s*=.*?,\s*\"(.*?)\",""")
+
+    let projLine line =
+        match projLineRe.Match line with
+        | m when m.Success -> Some m.Groups.[1].Value
+        | _ -> None
+
+    let fixDir (d : string) = d.Replace ('\\', Path.DirectorySeparatorChar)
+
+    let projectPaths =
+        let d = Path.GetDirectoryName (slnPath)
+        File.ReadAllLines slnPath
+        |> Seq.choose projLine
+        |> Seq.map (fun x -> Path.Combine (d, fixDir x))
+
+    let getProjectBin (projPath : string) : string =
+        let proj = XmlDocument ()
+        proj.Load projPath
+        let projDir = Path.GetDirectoryName projPath
+        let outType = (proj.GetElementsByTagName ("OutputType")).[0].InnerText.Trim ()
+        let ext = if outType = "Library" then ".dll" else ".exe"
+        let name = (proj.GetElementsByTagName ("AssemblyName")).[0].InnerText.Trim () + ext
+        let dirs =
+            (proj.GetElementsByTagName ("OutputPath"))
+            |> Seq.cast<XmlNode>
+            |> Seq.map (fun x -> x.InnerText.Trim ())
+        let paths =
+            dirs
+            |> Seq.map (fun x -> Path.Combine (projDir, fixDir x, name))
+        let fileInfos =
+            paths
+            |> Seq.map (fun x -> FileInfo (x))
+            |> Seq.sortBy (fun x -> x.LastWriteTime)
+            |> Array.ofSeq
+            |> Array.rev
+        (Seq.head fileInfos).FullName
+
+    let binPaths =
+        projectPaths
+        |> Seq.fold (fun bs path -> Set.add (getProjectBin path) bs) Set.empty
+
+    binPaths
+    |> Seq.collect diagnoseBinary
+    |> Seq.sortBy (fun x -> x.DeclaringType.FullName + x.Name) |> List.ofSeq
+
+
+//
+// Entrypoint
+//
+
+let argv = fsi.CommandLineArgs |> Seq.skip 1 |> Array.ofSeq
+
+if argv.Length < 1 then
+    printfn "StopCrashing.exe [Solution.sln]"
+    1
+else
+    
+    let slnPath = argv.[0]
+    let badMethods = diagnoseSln slnPath
+
+    if badMethods.Length = 0 then
+        Console.ForegroundColor <- ConsoleColor.Green
+        printfn "GOOD JOB WITH %s!" slnPath
+    else
+        printfn "%d UI METHODS IN %s NEED EXCEPTION HANDLERS" badMethods.Length slnPath
+        Console.ForegroundColor <- ConsoleColor.Red
+        for t in badMethods do
+            printfn "    %s.%s" t.DeclaringType.FullName t.Name
+
+    if badMethods.Length > 0 then 2 else 0
+
+
